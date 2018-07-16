6fe7f50f80064fe130751bfb625a74b86ae96e48
aritra <aritrakundu99@gmail.com>
2018-07-16 16:11:06 +0530

    Added Items page

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -13,6 +13,10 @@ app.config([ '$routeProvider', function($routeProvider) {
 		templateUrl : 'items.html',
 		controller : 'itemsCtrl'
 	});
+	$routeProvider.when('/login', {
+		templateUrl : 'login.html',
+		controller : 'loginCtrl'
+	});
 	$routeProvider.otherwise({
 		redirectTo : '/home'
 	});
@@ -55,6 +59,66 @@ app.controller("restaurantCtrl", function($scope, $http) {
 
 app.controller("itemsCtrl", function($scope, $http) {
 
+	$scope.fetchItem = function() {
+		$http({
+			method : 'GET',
+			url : 'http://localhost:8080/items/getAllItem'
+		}).success(function(data, status) {
+			console.log(data);
+			$scope.status = status;
+			$scope.items = data;
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.data = "Request failed";
+		});
+	};
+	
+	$scope.saveItem = function() {
+		$http({
+			method : 'POST',
+			url : 'http://localhost:8080/restaurant/addItem',
+			headers : {'Content-Type' : 'application/json' },
+			data : $scope.item
+		}).success(function(data, status) {
+			console.log(data);
+			$scope.fetchItem();
+			$scope.items = data;
+		}).error(function(data, status){
+			$scope.status = status;
+			$scope.data = "Request failed";
+		});
+		
+	};
 	
 
-})
+});
+
+
+app.controller("loginCtrl", function($scope, $http){
+	
+	$scope.loginUser = function(){
+	
+		$http({
+			method : 'POST',
+			url : 'http://localhost:8080/user/login',
+			headers: { 'Content-Type': 'application/json' },
+			data:$scope.user
+		}).success(function(data, status) {
+			console.log(data);
+			if(data){
+				$scope.message = "Welcome Back:" + data.name;
+				console.log($scope.lgText);
+				lgNam =  "Welcome Back:" + data.name;
+				console.log(lgNam);
+				window.location = "/#/items"
+			}else{
+				$scope.message = "Login Failed";
+			}
+		}).error(function(data, status) {
+			$scope.status = status;
+			console.log(data);
+			$scope.message = "Request failed";
+		});
+	};
+
+});
